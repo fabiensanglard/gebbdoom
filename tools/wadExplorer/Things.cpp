bd534a1bf438559542f06072c83539745873678f
fabien sanglard <fabien.sanglard@gmail.com>
2018-01-04 18:05:31 -0800

    Pretty map printing

---
+++ tools/wadExplorer/Things.cpp
@@ -0,0 +1,35 @@
+//
+// Created by fabien sanglard on 2017-12-24.
+//
+
+#include "Things.h"
+
+std::unordered_map<int, ThingDef> thingsDefs = {
+    // Monsters
+    {68, {2, 64, "BSPI", Class::Monster}},  // Arachnotron
+    {64, {2, 20, "VILE", Class::Monster}}, // Arch-Vile
+    {3003, {2, 24, "BOSS", Class::Monster}}, // Baron of Hell
+    {3005, {2, 31, "HEAD", Class::Monster}}, // Cacodemon
+    {65, {2, 20, "CPOS", Class::Monster}}, // Chaingunner
+    {72, {2, 16, "KEEN", Class::Monster}}, // Commander Keen
+    {16, {2, 40, "CYBR", Class::Monster}}, // Cyberdemon
+    {3002, {2, 30, "SARG", Class::Monster}}, // Demon
+    {3004, {2, 20, "POSS", Class::Monster}}, // Former Human Trooper
+    {9, {2, 20, "SPOS", Class::Monster}}, // Sergeant
+    {69, {2, 24, "BOS2", Class::Monster}}, // Hell Knight
+    {3001, {2, 20, "TROO", Class::Monster}}, // Imp
+    {3006, {2, 16, "SKUL", Class::Monster}}, // Lost Soul
+    {67, {2, 48, "FATT", Class::Monster}}, // Mancubus
+    {71, {2, 31, "PAIN", Class::Monster}}, // ain Elemental
+    {66, {2, 20, "SKEL", Class::Monster}}, // Revenant
+    {58, {2, 30, "SARG", Class::Monster}}, // Spectre
+    {7, {2, 128, "SPID", Class::Monster}}, // Spider Mastermind
+    {84, {2, 20, "SSWV", Class::Monster}}  // Wolfenstein SS
+};
+
+bool isMonster(size_t type) {
+    if (thingsDefs.find(type) == thingsDefs.end()) {
+        return false;
+    }
+    return thingsDefs[type].klass == Class::Monster;
+}
---
+++ tools/wadExplorer/Things.h
@@ -0,0 +1,36 @@
+//
+// Created by fabien sanglard on 2017-12-24.
+//
+
+#ifndef WADEXPLORER_THINGS_H
+#define WADEXPLORER_THINGS_H
+
+#include <cstdint>
+#include <unordered_map>
+#include <string>
+
+#define THING_LEVEL1_2         0x0001
+#define THING_LEVEL3           0x0002
+#define THING_LEVEL4_5         0x0004
+#define THING_DEAF             0x0008
+#define THING_NO_SINGLE_PLAYER 0x0010
+
+enum Class {Artifact, Pickup, Weapon, Monster, Obstacle, Hang};
+
+struct ThingDef {
+    ThingDef(): sprite("") {
+    }
+
+    ThingDef(uint8_t v, uint8_t r, std::string s, Class k) :  version(v), radius(r), sprite(s), klass(k) {}
+
+
+    uint8_t version;
+    uint8_t radius;
+    std::string sprite;
+    Class klass;
+};
+
+bool isMonster(size_t type);
+extern std::unordered_map<int,ThingDef> thingsDefs;
+
+#endif //WADEXPLORER_THINGS_H
