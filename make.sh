59b5def17baec7e6248a10e0c7a90b6ed8410f51
fabien sanglard <fabien.sanglard@gmail.com>
2018-01-14 20:10:58 -0800

    Smarter eps to pdf conversion with -nt

--- make.sh
+++ make.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+#set -x
+epsToPDF () {	
+    # Build path without extension.
+	filename=$(basename "$1")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+	dir=$(dirname $1)
 
-epsToPDF () {
-	echo $1
-    epstopdf $1
+	extensionless=$dir/$filename
+
+    # -nt = newer than. -ot = older than.
+	if [ "$extensionless".eps -nt "$extensionless".pdf ]; then
+		echo "Need to convert $extensionless.eps to PDF "
+        epstopdf "$extensionless".eps
+    fi
+    # epstopdf $filename
 }
 
 if [ ! -d output ]
@@ -12,7 +24,7 @@ fi
 cd src
 
 # Convert eps to pdf if necessary
-find . -name "*.eps" -exec epsToPDF('{}') \;
+find . -name "*.eps" | while read file; do epsToPDF "$file"; done
 
 # Compile 
 pdflatex -output-directory ../output book.tex
