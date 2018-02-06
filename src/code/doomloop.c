2e5fee4452672efa32bcb9b23e550142a0c5d8b7
Fabien Sanglard <sanglardf@google.com>
2018-02-06 00:16:38 -0800

    Worked on zone RAM and unrolled loops

---
+++ src/code/doomloop.c
@@ -0,0 +1,10 @@
+void D_DoomLoop (void) {
+	I_InitGraphics ();
+	while (1)
+	{                                                               
+		I_StartFrame ();   // frame syncronous IO operations
+		TryRunTics ();   // will run at least one tic
+		S_UpdateSounds (players[consoleplayer].mo);// move positional sounds
+		D_Display ();
+	}
+}
