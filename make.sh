b1d208afdc9e577c590dadcf7ca21c95d98affd1
Fabien Sanglard <sanglardf@google.com>
2018-02-23 22:20:02 -0800

    Added inkscape script generator

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#set -x
+export INKSCAPE=/Applications/Inkscape.app/Contents/Resources/bin/inkscape
+# set -x
 epsToPDF () {	
     # Build path without extension.
 	filename=$(basename "$1")
@@ -9,12 +10,41 @@ epsToPDF () {
 
 	extensionless=$dir/$filename
 
-    # -nt = newer than. -ot = older than.
+  # -nt = newer than. -ot = older than.
 	if [ "$extensionless".eps -nt "$extensionless".pdf ]; then
-		echo "Need to convert $extensionless.eps to PDF "
-        epstopdf "$extensionless".eps
-    fi
-    # epstopdf $filename
+		echo "Convert $extensionless.eps to PDF."
+    epstopdf "$extensionless".eps
+  fi
+  # epstopdf $filename
+  exit 0
+}
+
+svgToPNG() {
+    # Build path without extension.
+  filename=$(basename "$1")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+  dir=$(dirname $1)
+
+  extensionless=$dir/$filename
+
+  src="`pwd`/screenshots_svg/$filename".svg
+  dst="`pwd`/screenshots/$filename".png
+  # Low RES assets
+  # -nt = newer than. -ot = older than.
+  if [ ${src} -nt ${dst} ]; then
+    echo "Convert $extensionless.svg to PNG (300dpi)."
+    ${INKSCAPE} --export-png=${dst} --without-gui --export-dpi=300 ${src} > /dev/null 2>&1
+  fi
+
+  dst="`pwd`/screenshots_300dpi/$filename".png
+  # High RES assets
+  # -nt = newer than. -ot = older than.
+  if [ ${src} -nt ${dst} ]; then
+    echo "Convert $extensionless.svg to PNG (600dpi)."
+    ${INKSCAPE} --export-png=${dst} --without-gui --export-dpi=600 ${src} > /dev/null 2>&1
+  fi
+
 }
 
 if [ ! -d output ]
@@ -22,6 +52,9 @@ then
    mkdir output
 fi
 cd src
+
+#Convert complex svg drawings to PNG.
+find screenshots_svg -name "*.svg" | while read file; do svgToPNG "$file"; done
 
 # Convert eps to pdf if necessary
 find . -name "*.eps" | while read file; do epsToPDF "$file"; done
