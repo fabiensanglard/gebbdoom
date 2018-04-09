690a1a11081fe0e9968c7089c826d8b5a0b3fdb8
Fabien Sanglard <sanglardf@google.com>
2018-04-08 19:33:25 -0700

    PC Chapter FINISHED

--- src/code/real_mode_c.c
+++ src/code/real_mode_c.c
@@ -1,9 +1,28 @@
-#include <stdio.h>
-#include <dos.h>
+void MM_Startup (void) {
+  int i;
+  unsigned long length;
+  void far      *start;
+  unsigned      segstart,seglength,endfree;
 
-int main (int argc ,char** argv ) {
-    far void * a = farmalloc(0) ;
-    // BLA BLA BLA
-    // BLA BLA BLA
-    // BLA BLA BLA        
+  // get all available near conventional memory segments
+  length=coreleft();
+  start = (void far *)(nearheap = malloc(length));
+
+  length -= 16-(FP_OFF(start)&15);
+  length -= SAVENEARHEAP;
+  seglength = length / 16;      // now in paragraphs
+  segstart = FP_SEG(start)+(FP_OFF(start)+15)/16;
+  MML_UseSpace (segstart,seglength);
+  mminfo.nearheap = length;
+
+  // get all available far conventional memory segments
+  length=farcoreleft();
+  start = farheap = farmalloc(length);
+  length -= 16-(FP_OFF(start)&15);
+  length -= SAVEFARHEAP;
+  seglength = length / 16;      // now in paragraphs
+  segstart = FP_SEG(start)+(FP_OFF(start)+15)/16;
+  MML_UseSpace (segstart,seglength);
+  mminfo.farheap = length;
+  mminfo.mainmem = mminfo.nearheap + mminfo.farheap;
 }
