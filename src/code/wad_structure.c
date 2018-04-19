acb20846380d1c7961803cbddd9263428d8e261c
Fabien Sanglard <sanglardf@google.com>
2018-04-19 14:01:57 -0700

    333 pages, Solid 140 pages. Now go to engine.

---
+++ src/code/wad_structure.c
@@ -0,0 +1,12 @@
+typedef struct {
+    char magicNumber[4];      // "IWAD" or "PWAD"
+    int32_t numDirectories;   // Num directories 
+    int32_t directoryOffset;  // Offset to directies
+} header;
+
+typedef struct { 
+    int32_t offset;           // Offset to lump 
+    int32_t size;             // Size of the lump
+    char name[8];             // Name of the lump
+} directoryEntry;
+
