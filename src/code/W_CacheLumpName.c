c178f67ce7ca5c692d92349051f865308becbfe4
Fabien Sanglard <sanglardf@google.com>
2018-05-05 18:07:27 -0700

    347 pages, 3D

--- src/code/W_CacheLumpName.c
+++ src/code/W_CacheLumpName.c
@@ -1,1 +1,17 @@
-void* W_CacheLumpName (char *name, int tag);
+void* W_CacheLumpNum(int lump, int tag) {
+  byte *ptr;
+  if ((unsigned)lump >= numlumps)
+    I_Error ("W_CacheLumpNum: %i >= numlumps",lump);
+        
+  if (!lumpcache[lump]){
+    // read the lump in
+    //printf ("cache miss on lump %i\n",lump);
+    ptr = Z_Malloc(W_LumpLength (lump), tag, &lumpcache[lump]);
+    W_ReadLump(lump, lumpcache[lump]);
+  } else {
+    //printf ("cache hit on lump %i\n",lump);
+    Z_ChangeTag(lumpcache[lump],tag);
+  }
+    
+  return lumpcache[lump];
+}
--- src/code/kernel_renderer.c
+++ src/code/kernel_renderer.c
@@ -1,4 +1,6 @@
 // doomdef.h
+#define SCREENWIDTH     320
+#define SCREENHEIGHT    200
 extern byte *screens[5];
 
 // v_video.c
@@ -6,5 +8,6 @@ byte* base = I_AllocLow (SCREENWIDTH*SCREENHEIGHT*4);
 for (i=0 ; i<4 ; i++)
     screens[i] = base + i*SCREENWIDTH*SCREENHEIGHT;
 
+// st_stuff.c
 screens[4] = (byte *) Z_Malloc(ST_WIDTH*ST_HEIGHT, PU_STATIC, 0);
 
