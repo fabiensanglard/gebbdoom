c9a76afed04939f567be019414cdd6255cd9b3b5
Fabien Sanglard <sanglardf@google.com>
2018-05-09 15:37:09 -0700

    Done with walls. Onto visplanes

--- src/code/visplanes.c
+++ src/code/visplanes.c
@@ -1,15 +1,16 @@
 // Now what is a visplane, anyway?
 typedef struct {
-  fixed_t		height;
-  int			picnum;
-  int			lightlevel;
-  int			minx;
-  int			maxx;
+  fixed_t    height;
+  int        picnum;
+  int        lightlevel;
+  int        minx;
+  int        maxx;
 
-  byte		top[SCREENWIDTH];
-  byte		bottom[SCREENWIDTH];
+  byte       top[SCREENWIDTH];
+  byte       bottom[SCREENWIDTH];
 } visplane_t;
 
 // Here comes the obnoxious "visplane".
-#define MAXVISPLANES	128
-visplane_t		visplanes[MAXVISPLANES];
+#define MAXVISPLANES    128
+visplane_t   visplanes[MAXVISPLANES];
+visplane_t*  nextvisplanes;
