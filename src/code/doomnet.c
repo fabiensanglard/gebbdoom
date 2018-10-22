317d7b28054c2036dd2c328cb1a1dd2be218dc45
Fabien Sanglard <sanglardf@google.com>
2018-10-22 09:05:38 -0700

    Feedback from Jim Leonard2

--- src/code/doomnet.c
+++ src/code/doomnet.c
@@ -1,4 +1,3 @@
 
 C:\DOOM> DOOM.EXE -net 54359695
 
-
--- src/code/ticcmd_t.c
+++ src/code/ticcmd_t.c
@@ -1,4 +1,7 @@
-
+// The data sampled per tick (single player)
+// and transmitted to other peers (multiplayer).
+// Mainly movements/button commands per game tick,
+// plus a checksum for internal state consistency.
 typedef struct { 
   char            forwardmove;   // *2048 for move 
   char            sidemove;      // *2048 for move 
@@ -6,5 +9,4 @@ typedef struct {
   short           consistancy;   // checks for net game 
   unsigned char   chatchar; 
   unsigned char   buttons; 
-} ticcmd_t; 
-
+} ticcmd_t;
