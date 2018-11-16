d59bdcb4c094e2c6f37f30b15fe3d4969ec8a592
Fabien Sanglard <sanglardf@google.com>
2018-11-16 00:41:17 -0800

    The last proofreading. Done up to 'Drawing Flats'

--- src/code/W_CacheLumpName.c
+++ src/code/W_CacheLumpName.c
@@ -3,8 +3,7 @@ void* W_CacheLumpNum(int lump, int tag) {
   if ((unsigned)lump >= numlumps)
     I_Error ("W_CacheLumpNum: %i >= numlumps",lump);
         
-  if (!lumpcache[lump]){
-    // read the lump in
+  if (!lumpcache[lump]) {
     //printf ("cache miss on lump %i\n",lump);
     ptr = Z_Malloc(W_LumpLength (lump), tag, &lumpcache[lump]);
     W_ReadLump(lump, lumpcache[lump]);
